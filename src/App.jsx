ceadc0affea8b297941e7a8bc0460ec1c5ce6da8
Ebisu <127163361+aabyess@users.noreply.github.com>
2025-04-08 15:49:57 +0900

    0408
    라우터기능 구현

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,14 @@ import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 import data from './data.js';
-
+import { Routes, Route, Link, useNavigate, Outlet } from 'react-router-dom';
+import Detail from './routes/Detail.jsx';
+import Event from './routes/Event.jsx';
 
 function App() {
 
   let [shoes] = useState(data);
-
+  let navigate =  useNavigate();
   return (
     <div className='App'>
       <Navbar expand="lg" className="bg-body-tertiary">
@@ -19,8 +21,8 @@ function App() {
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="me-auto">
-              <Nav.Link href="#home">Home</Nav.Link>
-              <Nav.Link href="#link">Link</Nav.Link>
+              <Nav.Link onClick={()=>{ navigate(-1) }} href="#home">Home</Nav.Link>
+              <Nav.Link onClick={()=>{ navigate('/detail') }}>Detail</Nav.Link>
               <NavDropdown title="Dropdown" id="basic-nav-dropdown">
                 <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
                 <NavDropdown.Item href="#action/3.2">
@@ -36,30 +38,59 @@ function App() {
           </Navbar.Collapse>
         </Container>
       </Navbar>
-      <div className='main-bg'></div>
-      <div className="container">
-        <div className="row">
-          {
-            shoes.map((a, i) => {
-              return (
-                <Card shoes={shoes[i]} i={i}></Card>
-              )
-            })
-          }
-          {/* <Card shoes={shoes[0]} i={1}></Card>
-          <Card shoes={shoes[1]} i={2}></Card>
-          <Card shoes={shoes[2]} i={3}></Card> */}
-        </div>
-      </div>
+      {/* <Link to="/">홈</Link><br />
+      <Link to="/detail">상세페이지</Link> */}
+      <Routes>
+        <Route path='/' element={
+          <>
+            <div className='main-bg'></div>
+            <div className="container">
+              <div className="row">
+                {
+                  shoes.map((a, i) => {
+                    return (
+                      <Card shoes={shoes[i]} i={i}></Card>
+                    )
+                  })
+                }
+                {/* <Card shoes={shoes[0]} i={1}></Card>
+                    <Card shoes={shoes[1]} i={2}></Card>
+                    <Card shoes={shoes[2]} i={3}></Card> */}
+              </div>
+            </div>
+          </>
+        } />
+        <Route path='/detail' element={<Detail/>} />
+        <Route path='/about' element={<About/>}>
+          <Route path='member' element={<div>멤버임</div>} />
+          <Route path='location' element={<div>위치정보임</div>} />
+        </Route>
+        <Route path='/event' element={<Event/>}>
+          <Route path='one' element={<div>첫 주문시 양배추즙 서비스</div>}></Route>
+          <Route path='two' element={<div>생일기념 쿠폰받기</div>}></Route>
+        </Route>
+        <Route path='*' element={<div>없는페이지요</div>} />
+      </Routes>
+
     </div>
 
   )
 }
+function About(){
+  return (
+    <div>
+      <h4>회사정보임</h4>
+      <Outlet></Outlet>
+    </div>
+  )
+}
+
+
 
 function Card(props) {
   return (
     <div className="col-md-4">
-      <img src={'https://codingapple1.github.io/shop/shoes' + (props.i+1) + '.jpg'} width="80%" />
+      <img src={'https://codingapple1.github.io/shop/shoes' + (props.i + 1) + '.jpg'} width="80%" />
       <h4>{props.shoes.title}</h4>
       <p>{props.shoes.price}</p>
     </div>
