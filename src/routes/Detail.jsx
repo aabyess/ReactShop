943d13d73d2aaaeefac8071a4e7d3844e93027de
Ebisu <127163361+aabyess@users.noreply.github.com>
2025-04-09 15:56:56 +0900

    0409
    리액트 라우터기능 스타일-컴포넌트 배움

--- src/routes/Detail.jsx
+++ src/routes/Detail.jsx
@@ -1,14 +1,30 @@
-function Detail() {
+import { useParams } from "react-router-dom";
+import styled from "styled-components";
+
+let YellowBtn = styled.button`
+    background : ${props => props.bg};
+    color : ${props => props.bg == 'blue' ? 'white' : 'black'};
+    padding : 10px;
+`
+
+let NewBtn = styled.button(YellowBtn)
+
+function Detail(props) {
+
+    let { id } = useParams(); //현재 url주소에 입력한 숫자를 가져옴 
+
     return (
         <div className="container">
+                <YellowBtn bg="blue">버튼</YellowBtn>
+                <YellowBtn bg="orange">버튼</YellowBtn>
             <div className="row">
                 <div className="col-md-6">
                     <img src="https://codingapple1.github.io/shop/shoes1.jpg" width="100%" />
                 </div>
                 <div className="col-md-6">
-                    <h4 className="pt-5">상품명</h4>
-                    <p>상품설명</p>
-                    <p>120000원</p>
+                    <h4 className="pt-5">{props.shoes[id].title}</h4>
+                    <p>{props.shoes[id].content}</p>
+                    <p>{props.shoes[id].price}원</p>
                     <button className="btn btn-danger">주문하기</button>
                 </div>
             </div>
