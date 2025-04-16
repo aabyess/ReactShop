e2bc388777c3a45ee6336b0a9746f05fde0cb4e2
abyss <a00411@naver.com>
2025-04-16 19:25:53 +0900

    0416(3)
    Redux 사용하면 컴포넌트들이 props 없이 state 공유가능, 셋팅

--- src/App.jsx
+++ src/App.jsx
@@ -9,11 +9,13 @@ import { Routes, Route, Link, useNavigate, Outlet } from 'react-router-dom';
 import Detail from './routes/Detail.jsx';
 import Event from './routes/Event.jsx';
 import axios from 'axios';
+import Cart from './routes/Cart.jsx';
+
 
 function App() {
 
   let [shoes, setShoes] = useState(data);
-  let navigate =  useNavigate();
+  let navigate = useNavigate();
   return (
     <div className='App'>
       <Navbar expand="lg" className="bg-body-tertiary">
@@ -22,8 +24,8 @@ function App() {
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="me-auto">
-              <Nav.Link onClick={()=>{ navigate(-1) }} href="#home">Home</Nav.Link>
-              <Nav.Link onClick={()=>{ navigate('/detail') }}>Detail</Nav.Link>
+              <Nav.Link onClick={() => { navigate(-1) }} href="#home">Home</Nav.Link>
+              <Nav.Link onClick={() => { navigate('/detail') }}>Detail</Nav.Link>
               <NavDropdown title="Dropdown" id="basic-nav-dropdown">
                 <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
                 <NavDropdown.Item href="#action/3.2">
@@ -59,39 +61,44 @@ function App() {
                     <Card shoes={shoes[2]} i={3}></Card> */}
               </div>
             </div>
-            
-            <button onClick={()=>{
+
+            <button onClick={() => {
               /* ajax를 이용해서 서버에서 데이터를 가져옴 */
               axios.get('https://codingapple1.github.io/shop/data2.json')
-              .then((결과)=>{ 
-                /* 버튼을 클릭하면 shoes 데이터를 추가함 */
-                console.log(결과.data)
-                let copy = [...shoes, ...결과.data]; 
-                setShoes(copy);
-                /* Array 같은 데이터는 복사본을 만들어줘라 */
-                /* 괄호를 벗겨주는 기능 '...' */
-               })
+                .then((결과) => {
+                  /* 버튼을 클릭하면 shoes 데이터를 추가함 */
+                  console.log(결과.data)
+                  let copy = [...shoes, ...결과.data];
+                  setShoes(copy);
+                  /* Array 같은 데이터는 복사본을 만들어줘라 */
+                  /* 괄호를 벗겨주는 기능 '...' */
+                })
 
-               axios.post('/qwer',{ name:'kim' })
-               /* 서버로 데이터전송하는 POST요청 */
+              axios.post('/qwer', { name: 'kim' })
+                /* 서버로 데이터전송하는 POST요청 */
 
 
-              .catch(()=>{
-                console.log('실패함 ㅅㄱ') /* 실패 했을때 예외처리*/
-              })
+                .catch(() => {
+                  console.log('실패함 ㅅㄱ') /* 실패 했을때 예외처리*/
+                })
             }}>버튼</button>
           </>
         } />
-        <Route path='/detail/:id' element={<Detail shoes ={shoes}/>} />
 
+        {/* <Route path='/detail' element={<Detail shoes={shoes} />} /> */}
+        <Route path='/detail/:id' element={
+          <Detail shoes={shoes} />}
+        />
+        <Route path="/cart" element={
+          <Cart />}
+        />
 
-        
         {/* 상세페이지 */}
-        <Route path='/about' element={<About/>}>
+        <Route path='/about' element={<About />}>
           <Route path='member' element={<div>멤버임</div>} />
           <Route path='location' element={<div>위치정보임</div>} />
         </Route>
-        <Route path='/event' element={<Event/>}>
+        <Route path='/event' element={<Event />}>
           <Route path='one' element={<div>첫 주문시 양배추즙 서비스</div>}></Route>
           <Route path='two' element={<div>생일기념 쿠폰받기</div>}></Route>
         </Route>
@@ -102,7 +109,7 @@ function App() {
 
   )
 }
-function About(){
+function About() {
   return (
     <div>
       <h4>회사정보임</h4>
